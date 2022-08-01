5ec012242ca3330aa89175fa1d852664ab362ee6
Winner Musole <winnermusole@Winners-MacBook-Pro.local>
2022-08-01 13:54:21 -0700

    Added Transaction Debt and Orser service

---
+++ transaction_service/src/app.ts
@@ -0,0 +1,20 @@
+import express, { NextFunction, Request, Response } from "express";
+import { json } from "body-parser";
+
+const app = express();
+app.set("trust proxy", true)
+
+app.use(json());
+
+app.get("/api/users", (req: Request, res: Response) => {
+    res.status(200).send({"message": "Hello Kaka Derick"})
+})
+app.use((_, res) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+export {app}
