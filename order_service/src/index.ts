62acdf62f28c592e1bdb5cddcf7d1b515ddf82f3
Winner Musole <winnermusole@Winners-MacBook-Pro.local>
2022-08-08 12:01:37 -0700

    debt service, order service implementation

--- order_service/src/index.ts
+++ order_service/src/index.ts
@@ -3,7 +3,7 @@ import config from "./config/config"
 import {logging} from "./config/logging";
 
 const start = async () => {
-    app.listen(config.server.port, () => logging.info(`Debt Server is running ${config.server.host}:${config.server.port}`));
+    app.listen(config.server.port, () => logging.info(`Order Server is running ${config.server.host}:${config.server.port}`));
 }
 
 start();
